9beea184c04a453ca92011dcfc7e7721f6f7068f
Abdallahkajj <busy.heart@live.com>
2021-03-18 12:46:58 +0300

    Controller

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using SchoolManagement.Business;
+using SchoolManagement.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;

3e5293489bf080c30ad5e7212695ae289c7af8b0
Abdallahkajj <busy.heart@live.com>
2021-03-23 17:56:44 +0300

    NLog Added

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace WebAPI
 {
@@ -21,6 +23,10 @@ namespace WebAPI
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                }).ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                }).UseNLog();
     }
 }
